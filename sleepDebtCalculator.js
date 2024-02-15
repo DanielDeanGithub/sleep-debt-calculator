44143b165c6cc4a545859209ec043324bd0ab44c
DanielDeanGithub <github-new@inboxrefresh.com>
2024-02-15 08:57:10 +0000

    Added switch statement to getSleepHour function

--- sleepDebtCalculator.js
+++ sleepDebtCalculator.js
@@ -1,3 +1,30 @@
-const getSleepHours = day => {
-    
-}
+const getSleepHour  = day => {
+    day = day.toLowerCase();
+
+    switch (day) {
+        case "monday":
+            return 6
+            break;
+        case "tuesday":
+            return 7
+            break;
+        case "wednesday":
+            return 6
+            break;
+        case "thursday":
+            return 6
+            break;
+        case "friday":
+            return 7
+            break;
+        case "saturday":
+            return 8
+            break;
+        case "sunday":
+            return 6
+            break;
+        default:
+            console.log("Error: Invalid input")
+            break;
+    }
+};
